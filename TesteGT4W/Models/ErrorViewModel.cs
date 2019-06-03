b7b71b84913c3aaf9c77b1f0610b89ac45f7d4e5
Thiago Santos <thiagohillsong@hotmail.com>
2019-06-03 13:01:28 -0300

    Initial Commit

---
+++ TesteGT4W/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TesteGT4W.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
