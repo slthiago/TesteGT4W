6bb48066b03f2f052f7a081c3c0f184e4a646ccc
Thiago Santos <thiagohillsong@hotmail.com>
2019-06-10 18:35:04 -0300

    Commit final

--- TesteGT4W/Models/Pessoa.cs
+++ TesteGT4W/Models/Pessoa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,15 @@ namespace TesteGT4W.Models
     public class Pessoa
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
         public string Cpf { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
         public DateTime DateNascimento { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
         public double Peso { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
         public string Uf { get; set; }
     }
 }
